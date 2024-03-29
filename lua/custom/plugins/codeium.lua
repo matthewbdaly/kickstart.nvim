12e6e77298c678e04bbeca11597cdfb410dffc4a
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2024-03-29 19:24:34 +0000

    Enabled Codeium

---
+++ lua/custom/plugins/codeium.lua
@@ -0,0 +1,22 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  'Exafunction/codeium.vim',
+  config = function()
+    -- Change '<C-g>' here to any keycode you like.
+    vim.keymap.set('i', '<C-g>', function()
+      return vim.fn['codeium#Accept']()
+    end, { expr = true, silent = true })
+    vim.keymap.set('i', '<c-;>', function()
+      return vim.fn['codeium#CycleCompletions'](1)
+    end, { expr = true, silent = true })
+    vim.keymap.set('i', '<c-,>', function()
+      return vim.fn['codeium#CycleCompletions'](-1)
+    end, { expr = true, silent = true })
+    vim.keymap.set('i', '<c-x>', function()
+      return vim.fn['codeium#Clear']()
+    end, { expr = true, silent = true })
+  end,
+}
