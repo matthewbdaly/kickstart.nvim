90862003132aafa53fd67a0b8dd02779be6b035d
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2024-06-13 12:58:54 +0100

    Configured Conform

--- init.lua
+++ init.lua
@@ -694,7 +694,8 @@ require('lazy').setup({
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        javascript = { { 'prettierd', 'prettier' } },
+        javascript = { 'eslint_d' },
+        php = { 'phpcbf' },
       },
     },
   },
