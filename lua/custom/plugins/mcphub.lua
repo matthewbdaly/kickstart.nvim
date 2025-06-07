6f50df95b6fbff35761432918edf36d9f01aa399
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2025-06-07 22:34:35 +0100

    Added MCPHub config

---
+++ lua/custom/plugins/mcphub.lua
@@ -0,0 +1,16 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'ravitemer/mcphub.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    build = 'npm install -g mcp-hub@latest', -- Installs `mcp-hub` node binary globally
+    config = function()
+      require('mcphub').setup()
+    end,
+  },
+}
