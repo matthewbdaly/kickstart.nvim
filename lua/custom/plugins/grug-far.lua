486248e4ed217e6953f9e691e878c329dd23c265
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2025-06-21 11:45:36 +0100

    Installed grug-far

---
+++ lua/custom/plugins/grug-far.lua
@@ -0,0 +1,19 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'MagicDuck/grug-far.nvim',
+    -- Note (lazy loading): grug-far.lua defers all it's requires so it's lazy by default
+    -- additional lazy config to defer loading is not really needed...
+    config = function()
+      -- optional setup call to override plugin options
+      -- alternatively you can set options with vim.g.grug_far = { ... }
+      require('grug-far').setup {
+        -- options, see Configuration section below
+        -- there are no required options atm
+      }
+    end,
+  },
+}
