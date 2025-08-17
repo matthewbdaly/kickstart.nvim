0fec02f1832f3d8c636abbddabb19f04544af8eb
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2025-08-17 12:02:57 +0100

    Added Obsidian integration

---
+++ lua/custom/plugins/obsidian.lua
@@ -0,0 +1,36 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'obsidian-nvim/obsidian.nvim',
+    version = '*', -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = 'markdown',
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+    --   -- refer to `:h file-pattern` for more examples
+    --   "BufReadPre path/to/my-vault/*.md",
+    --   "BufNewFile path/to/my-vault/*.md",
+    -- },
+    ---@module 'obsidian'
+    ---@type obsidian.config
+    opts = {
+      workspaces = {
+        {
+          name = 'DreamingSpires',
+          path = '~/Documents/ObsidianVaults/Dreaming Spires Bible/',
+        },
+        {
+          name = 'Metacivilization',
+          path = '~/Documents/ObsidianVaults/Metacivilization/',
+        },
+      },
+
+      -- see below for full list of options 👇
+    },
+  },
+}
